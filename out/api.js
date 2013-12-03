fbfd67ce442994644ea2085c7d520cf3ef683883
Olivier Kaisin <olivier.kaisin@gmail.com>
2013-12-03 17:43:42 +0100

    YUI Doc

---
+++ out/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "HttpError",
+        "Shooter"
+    ],
+    "modules": [
+        "mg42"
+    ],
+    "allModules": [
+        {
+            "displayName": "mg42",
+            "name": "mg42"
+        }
+    ]
+} };
+});
